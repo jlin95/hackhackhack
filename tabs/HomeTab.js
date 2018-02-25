84f69602d6853e57bb6b0133cfeca0d0c76f15c8
jlin95 <leeway95@gmail.com>
2018-02-25 12:02:31 +0800

    Ben's commit

--- tabs/HomeTab.js
+++ tabs/HomeTab.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, Button, Image } from 'react-native';
 import Colors from '../Colors';
 
 export default class HomeTab extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+      <Image style={styles.image} source={require('../assets/img/group313.png')}
+            />
         <Text style={styles.text}>Hooray! Your smart home devices are being secured</Text>
         <Text style={styles.textSmall}>Last threat scan: 0800 GMT +8 at 1/2/2018</Text>
         <Text style={styles.textSmall}>Smart Home Devices Status: <Text style={styles.status}>Protected</Text> </Text>
@@ -23,6 +25,12 @@ const styles = StyleSheet.create({
   container: {
     marginLeft: 40,
     marginRight: 40,
+  },
+  image: {
+    height: 200,
+    width: 272,
+    alignContent: 'center',
+    marginTop: 40,
   },
   text: {
     color: 'grey',
